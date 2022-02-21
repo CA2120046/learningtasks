6e4e4f91c4ef2c8240d43b2887df7081da1f451a
CA2120046 <100059427+CA2120046@users.noreply.github.com>
2022-02-21 14:04:24 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>my Home Page</title>
+</head>
+<body>
+  <h1>Welcome, I am Carl Agapito and this is my Website</h1>
+ <ul>
+ <li><a href="index.php">HOME </a> </li> 
+ <li><a href="/LT2/iindex.html"> LEARNING TASK 2 </a> </li>
+ </ul>
+ </body>
+ </html>
