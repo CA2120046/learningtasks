edc1ed5f3ecab29939f95c889d405b2cbd10fc73
CA2120046 <100059427+CA2120046@users.noreply.github.com>
2022-02-21 15:44:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
   <title>my Home Page</title>
 </head>
 <body>
-  <h1>Welcome, I am Carl Agapito and this is my Website</h1>
+  <h1>Welcome to My Website! I am Carl Agapito</h1>
  <ul>
- <li><a href="index.php">HOME </a> </li> 
- <li><a href="/LT2/index.html"> LEARNING TASK 2 </a> </li>
+    <li><a href="index.php">HOME </a></li> 
+    <li><a href="/LT2/index.html"> LEARNING TASK 2 </a></li>
  </ul>
  </body>
  </html>
