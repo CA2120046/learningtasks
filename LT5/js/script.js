be67e7eff62fa9ab2d75f2738c669fb63818922b
CA2120046 <100059427+CA2120046@users.noreply.github.com>
2022-05-16 21:56:28 +0800

    Add files via upload

---
+++ LT5/js/script.js
@@ -0,0 +1,17 @@
+function button1() {
+  document.getElementById('myimage').src='sunrise.jpg'>Press for Sunrise
+}
+function button1(){
+  document.getElementById('myimage').src='sunrise.jpg'>Press for Sunrise
+}
+function cndclick1(){
+  document.getElementById('myimage').src='sunrise.jpg'>Press for Sunrise
+}
+function button2() {
+  document.getElementById('myimage').src='sunset.jpg'>Press for Sunrise
+}
+function button2(){
+  document.getElementById('myimage').src='sunset.jpg'>Press for Sunrise
+}
+function cndclick2(){
+  document.getElementById('myimage').src='sunset.jpg'>Press for Sunrise
